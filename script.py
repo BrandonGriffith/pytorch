9ba715aea20754da910d93235520c4e561ed299f
BrandonGriffith <BrandonGriffith92@gmail.com>
2025-02-04 15:06:29 -0500

    update

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
 import torch
+import numpy as np
 
-x = torch.empty(1)
-print(x)
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    x = torch.ones(2, 2, device=device)
+    y = torch.ones(2, 2)
+    y = y.to(device)
+    z = x + y
+    print(z)
+    z = z.to("cpu")
+    print(z)
+
+a = z.numpy()
+print("numpy",a)
